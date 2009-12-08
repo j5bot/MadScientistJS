a7b45e78a2b6191f9cf562017d1bd4eb67f0d194
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2009-12-07 19:47:14 -0800

    converting tabs to spaces

--- js/jquery.madscience.js
+++ js/jquery.madscience.js
@@ -1,42 +1,42 @@
 (function (jQuery) {
-	
-	/* constructor */
-	function MadScience () {
-		"it's alive!";
-	}
-	
-	/* prototype */
-	MadScience.prototype = {
-		/*
-		A namespace tunneling function, which allows us to chain calls
-		e.g. jQuery(".foo").madscience().bar();
-		
-		without chaining, a call to jQuery(".foo").madscience.bar() would not
-		provide a jQuery object to the plugin method
-		
-		without the extend, we'd have a vanilla jQuery object and
-		no access to our child methods
-		*/
-		madscience: function () {
-			return jQuery.extend(this,arguments.callee,arguments.callee.prototype);
-		}
-	};
-	
-	/*
-	By using the constructor, prototype, extend/plugin sequence,
-	you can define related namespaces (perhaps henchmen?) in the prototype
-	
-	if you don't need that, you can also write the code as:
-	jQuery.fn.madscience = function () { return jQuery.extend(this,arguments.callee); }
-	
-	you can add to the madscience() namespace by referencing either
-	jQuery.fn.madscience (e.g. jQuery.fn.madscience.foo = function () {};)
-	which adds functions to the madscience function object or
-	jQuery.fn.madscience.prototype (e.g. jQuery.fn.madscience.prototype.foo)
-	which adds functions to the madscience object's prototype
-	
-	using the prototype is best practice, but either is acceptable
-	*/
-	jQuery.fn.extend(MadScience.prototype);
-		
+    
+    /* constructor */
+    function MadScience () {
+        "it's alive!";
+    }
+    
+    /* prototype */
+    MadScience.prototype = {
+        /*
+        A namespace tunneling function, which allows us to chain calls
+        e.g. jQuery(".foo").madscience().bar();
+        
+        without chaining, a call to jQuery(".foo").madscience.bar() would not
+        provide a jQuery object to the plugin method
+        
+        without the extend, we'd have a vanilla jQuery object and
+        no access to our child methods
+        */
+        madscience: function () {
+            return jQuery.extend(this,arguments.callee,arguments.callee.prototype);
+        }
+    };
+    
+    /*
+    By using the constructor, prototype, extend/plugin sequence,
+    you can define related namespaces (perhaps henchmen?) in the prototype
+    
+    if you don't need that, you can also write the code as:
+    jQuery.fn.madscience = function () { return jQuery.extend(this,arguments.callee); }
+    
+    you can add to the madscience() namespace by referencing either
+    jQuery.fn.madscience (e.g. jQuery.fn.madscience.foo = function () {};)
+    which adds functions to the madscience function object or
+    jQuery.fn.madscience.prototype (e.g. jQuery.fn.madscience.prototype.foo)
+    which adds functions to the madscience object's prototype
+    
+    using the prototype is best practice, but either is acceptable
+    */
+    jQuery.fn.extend(MadScience.prototype);
+        
 })(jQuery);
--- js/jquery.madscience.viewsource.js
+++ js/jquery.madscience.viewsource.js
@@ -1,21 +1,21 @@
 (function (jQuery) {
-	
-	function viewsource(e) {
-		var t = jQuery(this);
+    
+    function viewsource(e) {
+        var t = jQuery(this);
 
-		jQuery.ajax({
-			url: t.attr("href"),
-			success: function (response,status) {
-				t.after("<textarea rows=\"10\" cols=\"80\">"+response+"</textarea>");
-				t.unbind("click");
-			}
-		});
+        jQuery.ajax({
+            url: t.attr("href"),
+            success: function (response,status) {
+                t.after("<textarea rows=\"10\" cols=\"80\">"+response+"</textarea>");
+                t.unbind("click");
+            }
+        });
 
-		return false;
-	}
-	
-	jQuery.fn.madscience.prototype.viewsource = function () {
-		return this.click(viewsource);
-	};
-		
+        return false;
+    }
+    
+    jQuery.fn.madscience.prototype.viewsource = function () {
+        return this.click(viewsource);
+    };
+        
 })(jQuery);
